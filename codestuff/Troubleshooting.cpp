2974ad2410c6f52152c0baa94cc066f0b5d06f4a
Tupryk <68149355+Tupryk@users.noreply.github.com>
2022-12-05 13:37:27 +0100

    Delete codestuff directory

--- codestuff/Troubleshooting.cpp
+++
@@ -1,9 +0,0 @@
-// TROUBLESHOOTING
-
-void drawSquare(StaticBody2D staticBody, sf::Color color)
-{
-	sf::RectangleShape rectangle(sf::Vector2f(staticBody.size.x, staticBody.size.y));
-	rectangle.setFillColor(color);
-	rectangle.setPosition(staticBody.position.x - staticBody.size.x*0.5, staticBody.position.y - staticBody.size.y*0.5);
-	window.draw(rectangle);
-}
