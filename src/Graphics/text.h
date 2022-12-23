b237983a226916e4477b0fcdb4b411cc32901b8a
Eckart Cobo Briesewitz <eck4art@gmail.com>
2022-12-23 11:47:28 +0100

    visual text header (wont compile yet)

--- src/Graphics/text.h
+++ src/Graphics/text.h
@@ -4,7 +4,29 @@
 
 class Text
 {
+	sf::Font font;
+	sf::Text text;
+	sf::Text cursor;
+	std::string text;
+public:
+	void init(std::string font_file);
+	void update(std::string);
+	void draw(float x, float y, sf::RenderWindow* window, float box_w = 0, float box_h = 0);
+};
 
+class DynamicText
+{
+	sf::Font font;
+	sf::Text text;
+	sf::Text cursor;
+	sf::Clock SpellingTimer;
+	sf::SoundBuffer buffer;
+	sf::Sound sound;
+	std::string text;
+public:
+	void init(std::string font_file);
+	void update(std::string);
+	void draw(float x, float y, sf::RenderWindow* window, float box_w = 0, float box_h = 0);
 };
 
 
